d45540a8a65e835729302c265cd94f232f8266ae
Mike Tran <ngocmike238@gmail.com>
2021-01-12 00:51:04 -0500

    Update SlidingWindowMaximum (239)
    Using deque solution

--- src/SlidingWindow/239.py
+++ src/SlidingWindow/239.py
@@ -1,5 +1,5 @@
-# Node: Time Limit Exceeded
 class Solution(object):
+    # Note: Time Limit Exceeded
     def maxSlidingWindow(self, nums, k):
         max_t = max(nums[0:k])
         n = len(nums)
@@ -13,4 +13,23 @@ class Solution(object):
 
             ret.append(max_t)
         return ret
+    
+    #Deque solution but using default Python array
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        if not nums:
+            return []
+        if k == 0:
+            return nums
+        res = []
+        dq = []
+        for i in range(0,len(nums)):
+            while dq and nums[i] > nums[dq[-1]]:
+                dq.pop()
+            if dq and i - k >= dq[0]:
+                dq.pop(0)
+            dq.append(i)
+            if i >= k-1: # i == k-1 is the beginning of a full window
+                res.append(nums[dq[0]]) #the biggest number is always the first number in the queue
+            #print(dq)
+        return res
 
